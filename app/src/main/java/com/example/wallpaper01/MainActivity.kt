d9119f15bee15139c3c2b94f203d69d4fa75f0e7
kaushikk26 <kaushiksardhara91@gmail.com>
2024-01-31 20:59:29 +0530

    ok

--- app/src/main/java/com/example/wallpaper01/MainActivity.kt
+++ app/src/main/java/com/example/wallpaper01/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     var name_array= arrayOf("Car","Bike","Natur","Animal","Bird","Winter","Sumer","Super Hero","Flowers","Tourism")
     var array= arrayOf(R.drawable.car_wall,R.drawable.bike_wall,R.drawable.nature_wall,R.drawable.animal_wall,
-        R.drawable.bird_eye_wall,R.drawable.winter_wall,R.drawable.summer_wall,R.drawable.super_hero_wall,R.drawable.flower_wall,R.drawable.tourism_wall)
+        R.drawable.bird_wallpaper,R.drawable.winter_wall,R.drawable.summer_wall,R.drawable.super_hero_wall,R.drawable.flower_wall,R.drawable.tourism_wall)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
